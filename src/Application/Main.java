521311bf210e2d82aeed40844e83c712f57c20f4
Afonso Neves <afonsomaria127@gmail.com>
2023-09-29 21:30:58 +0100

    1ST vt

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -1,5 +1,6 @@
 package Application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Main {
@@ -7,6 +8,9 @@ public class Main {
 
         Position p1 = new Position(3,8);
         System.out.println(p1);
+        Board b1 = new Board(4,7);
+
+
 
 
     }
