fa9301847671df2384b691c8192f244da999a239
Afonso Neves <afonsomaria127@gmail.com>
2023-09-29 09:15:17 +0100

    First class: Position

---
+++ src/Application/Main.java
@@ -0,0 +1,13 @@
+package Application;
+
+import boardgame.Position;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Position p1 = new Position(3,8);
+        System.out.println(p1);
+
+
+    }
+}
